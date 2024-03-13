e000332dbf4b6c9f952d69f4eb0a498263de07fa
nguyenvanan2730 <nguyenvanan2730@gmail.com>
2024-03-13 22:41:04 +0900

    [Update] Added Auth
    Change the content in App.js and index.js
    Add auth allow sign in

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "amplifyreactgraphql4c25b1d7": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
